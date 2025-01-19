70d0d00b6a907390319f93d7b13a013b03d1a0d0
v.gromova <v.gromova@yadro.com>
2025-01-19 19:36:40 +0300

    Do rebase

--- BigInteger/test/BigIntTest.cpp
+++ BigInteger/test/BigIntTest.cpp
@@ -2,8 +2,6 @@
 
 #include <gtest/gtest.h>
 
-//TEST() arguments go from general to specific. The first argument is the name of the test suite, and the second argument is the test’s name within the test suite. 
-
 TEST(BigIntegerTest, can_create_matrix_with_positive_length)
 {
 	ASSERT_NO_THROW(BigInteger A("989890097097060"));
